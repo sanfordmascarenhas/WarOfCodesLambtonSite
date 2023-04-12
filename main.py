ebbe0236f8266a2b0e65eeba125b8952f96b9d95
Jessica Dsouza <jessicadsouza97@gmail.com>
2023-04-12 17:13:55 -0400

    Main Python script
    Contains connection with the mysql and initial render page

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
