97ed05ff410325dd791bd94dc64feff6070974b9
Sanford Mascarenhas <25857590+sanfordmascarenhas@users.noreply.github.com>
2023-04-12 18:35:38 -0400

    Added register and moved database files

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import os
 
 app = Flask(__name__)
 
 # Change this to your secret key (can be anything, it's for extra protection)
-# TODO: Add key here for later.
-app.secret_key = ''
+# Random 47 bytes for session key.
+app.secret_key = os.urandom(47)
 
 # Enter your database connection details below. Will be specific per system. 
 # Sync this across development systems.
@@ -47,3 +48,51 @@ def login():
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
+
+# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if account exists using MySQL
+        # Create the connection and check for the username
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        
+        # If account exists show error. Otherwise perform validation checks.
+        if account:
+            msg = 'Account already exists! Please try a new username'
+        # Check if the username has only numbers and characters.
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        # Fields are empty check.
+        elif not username or not password:
+            msg = 'Please complete the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+
+# http://localhost:5000/python/logout - this will be the logout page which will redirect to the login page anyway.
+@app.route('/socials/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
